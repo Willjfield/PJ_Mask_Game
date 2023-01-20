41323bd0a7d9ed12994e7952dacdd4947405ef0c
willjfield <willjfield@gmail.com>
2023-01-20 17:58:36 -0500

    pointer

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ function dragElement(elmnt) {
   
   //if (document.getElementById(elmnt.id + "header")) {
     // if present, the header is where you move the DIV from:
-    //document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    //document.getElementById(elmnt.id + "header").onpointerdown = dragMouseDown;
  // } else {
     // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
+    elmnt.onpointerdown = dragMouseDown;
   //}
 
   function dragMouseDown(e) {
@@ -21,9 +21,9 @@ function dragElement(elmnt) {
     // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
+    document.onpointerup = closeDragElement;
     // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
+    document.onpointermove = elementDrag;
   }
 
   function elementDrag(e) {
@@ -74,7 +74,7 @@ function dragElement(elmnt) {
 
   function closeDragElement() {
     // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
+    document.onpointerup = null;
+    document.onpointermove = null;
   }
 }
